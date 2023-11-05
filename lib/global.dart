08b8fbe0b896faef35e74107ba7019cd40ed1962
Nik Uzair <nikuzairsc@gmail.com>
2023-11-05 10:54:08 +0800

    Introducing bloc observer to the app

--- lib/global.dart
+++ lib/global.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yt_ulearning/common/routes/bloc_observer.dart';
 import 'package:yt_ulearning/common/service/storage_service.dart';
 
 class Global {
   static late StorageService storageService;
   static Future init() async {
     WidgetsFlutterBinding.ensureInitialized();
-
+    Bloc.observer = MyGlobalObserver();
     await Firebase.initializeApp();
 
     storageService = await StorageService().init();
